2dc32599cbdcb7d6e5fd5c682c04aae18cf00e31
David Duarte <37241920+daviddev16@users.noreply.github.com>
2021-09-16 20:01:58 -0300

    Update Main.java

--- src/Aula7/Main.java
+++ src/Aula7/Main.java
@@ -4,8 +4,12 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		Pessoa motorista = new Motorista("Motorista1", "ABC");
-		Pessoa passageiro = new Passageiro("Passageiro1", "ABC");
+		Motorista motorista = new Motorista("Motorista1", "ABC");
+		motorista.setHabilitacao("2191ABC");
+		motorista.setMatricula(291002);
+		
+		Passageiro passageiro = new Passageiro("Passageiro1", "ABC");
+		passageiro.setCodigoCliente(2910429);
 		
 		if(motorista instanceof Pessoa && passageiro instanceof Pessoa) {
 			System.out.println("Ambos são pessoas!");
